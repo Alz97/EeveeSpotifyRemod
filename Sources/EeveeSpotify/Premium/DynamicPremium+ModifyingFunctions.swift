18c47c3f8639b05125275654958ba2891b1e91f8
WADBB <kiki8.fs@gmail.com>
2026-05-24 18:23:40 +0200

    Update audio quality and high-bitrate attributes

--- Sources/EeveeSpotify/Premium/DynamicPremium+ModifyingFunctions.swift
+++ Sources/EeveeSpotify/Premium/DynamicPremium+ModifyingFunctions.swift
@@ -334,7 +334,11 @@ private func modifyAttributes(_ attributes: inout [String: AccountAttribute]) {
     formatter.timeZone = TimeZone(abbreviation: "UTC")
     
     attributes["audio-quality"] = AccountAttribute.with {
-        $0.stringValue = ""
+        $0.stringValue = "6"
+    }
+
+    attributes["high-bitrate"] = AccountAttribute.with {
+        $0.boolValue = true
     }
     
     attributes["ads"] = AccountAttribute.with {
