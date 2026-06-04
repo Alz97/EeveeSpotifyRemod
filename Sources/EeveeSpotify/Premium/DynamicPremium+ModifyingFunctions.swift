cfb0a8ebc0a7b9642f0ad1a50d2cbc10f1c9df8a
WADBB <kiki8.fs@gmail.com>
2026-06-04 21:00:26 +0200

    Change audio quality attribute value to empty string

--- Sources/EeveeSpotify/Premium/DynamicPremium+ModifyingFunctions.swift
+++ Sources/EeveeSpotify/Premium/DynamicPremium+ModifyingFunctions.swift
@@ -356,7 +356,7 @@ private func modifyAttributes(_ attributes: inout [String: AccountAttribute]) {
     }
     
     attributes["audio-quality"] = AccountAttribute.with {
-        $0.stringValue = "3"
+        $0.stringValue = ""
     }
 
     attributes["high-bitrate"] = AccountAttribute.with {
