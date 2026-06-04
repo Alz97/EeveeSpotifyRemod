da4d681870c3bebd2c90840f0feeb481c06fde3a
WADBB <kiki8.fs@gmail.com>
2026-06-04 19:10:44 +0200

    Update build number to 8

--- Sources/EeveeSpotify/Tweak.x.swift
+++ Sources/EeveeSpotify/Tweak.x.swift
@@ -200,7 +200,7 @@ func eeveeEnvFlag(_ name: String) -> Bool {
 
 struct EeveeSpotify: Tweak {
     static let version = "6.7.1"
-    static let buildNumber = "0"
+    static let buildNumber = "8"
     static let repoSlug = GeneratedConfig.repoSlug
     
     static var hookTarget: VersionHookTarget {
