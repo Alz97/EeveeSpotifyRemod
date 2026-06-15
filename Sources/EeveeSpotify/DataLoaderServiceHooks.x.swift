f3b954e5b7cbc9097003a109f1eba2169b3aa559
WADBB <kiki8.fs@gmail.com>
2026-06-15 04:01:06 +0200

    Increase semaphore wait timeout to 10 seconds

--- Sources/EeveeSpotify/DataLoaderServiceHooks.x.swift
+++ Sources/EeveeSpotify/DataLoaderServiceHooks.x.swift
@@ -80,7 +80,7 @@ class SPTDataLoaderServiceHook: ClassHook<NSObject>, SpotifySessionDelegate {
                     semaphore.signal()
                 }
 
-                _ = semaphore.wait(timeout: .now() + .milliseconds(5000))
+                _ = semaphore.wait(timeout: .now() + .milliseconds(10000))
                 orig.URLSession(session, dataTask: task, didReceiveData: customLyricsData ?? buffer)
                 orig.URLSession(session, task: task, didCompleteWithError: nil)
                 return
